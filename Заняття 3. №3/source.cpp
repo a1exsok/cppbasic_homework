8ed1d2cbbd9aead15877a84e659979e28d786247
readonly <admin@sokteam.com>
2023-10-15 19:23:57 +0200

    added Lesson 3. Task 3

---
+++ Заняття 3. №3/source.cpp
@@ -0,0 +1,19 @@
+﻿/*
+3) Дано ціна 1 хвилини та кількість хвилин. Обчислити вартість дзвінка.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// Currency in USD
+	int priceForOneMinute = 2;
+	int minutes = 50;
+
+	std::cout << "The price of one minute was " << priceForOneMinute << " USD." << std::endl;
+	std::cout << "The overall call time was " << minutes << " minutes." << std::endl;
+	std::cout << "The price for that call was " << priceForOneMinute * minutes << " USD." << std::endl;
+
+	return 0;
+}
