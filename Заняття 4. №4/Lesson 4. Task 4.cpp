3ad388316e1071ae1d67cff5acd3911690ee70cc
readonly <admin@sokteam.com>
2023-10-16 20:33:28 +0200

    Lesson 5. Task 5

---
+++ Заняття 4. №4/Lesson 4. Task 4.cpp
@@ -0,0 +1,25 @@
+﻿/*
+Lesson 4 | 4) Дано 3 числа. Вивести true,
+якщо серед них є додатні та парні.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = -3, b = 2, c = 7;
+
+    if (a > 0 || a / 2 == 0) {
+        cout << "true";
+    }
+    else if (b > 0 || b / 2 == 0) {
+        cout << "true";
+    }
+    else if (c > 0 || c / 2 == 0) {
+        cout << "true";
+    }
+
+    return 0;
+}
+
