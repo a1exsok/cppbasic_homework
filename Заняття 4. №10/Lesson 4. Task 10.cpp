f4d937e1b44dd7240f3fa5a0d00c65feaeea5358
readonly <admin@sokteam.com>
2023-10-16 21:46:04 +0200

    Lesson 4. Task 10.

---
+++ Заняття 4. №10/Lesson 4. Task 10.cpp
@@ -0,0 +1,24 @@
+﻿/*
+Lesson 4 | 10) Дано 2 числа. Якщо перше число більше другого,
+поміняти місцями значення змінних. Вивести значення змінних на екран.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n1 = 12, n2 = 8, tmp = 0;
+
+    cout << "Initially,\nn1 = " << n1 << "\nn2 = " << n2 << endl;
+    cout << "====" << endl;
+    if (n1 > n2) {
+        tmp = n2;
+        n2 = n1;
+        n1 = tmp;
+        cout << "Eventually,\nn1 = " << n1 << "\nn2 = " << n2 << endl;
+    }
+    else cout << "Eventually,\nn1 = " << n1 << "\nn2 = " << n2 << endl;
+
+    return 0;
+}
