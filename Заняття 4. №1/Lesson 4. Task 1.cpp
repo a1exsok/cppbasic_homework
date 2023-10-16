d7ad9b2316ffc7cdd59608b6d73c305143492fb3
readonly <admin@sokteam.com>
2023-10-16 18:24:22 +0200

    lesson 4. task 1

---
+++ Заняття 4. №1/Lesson 4. Task 1.cpp
@@ -0,0 +1,23 @@
+﻿/*
+Lesson 4 | 1) Написати програму, яка знаходить та виводить в дійсному вигляді
+середнє арифметичне трьох введених ЦІЛИХ чисел. Запропонувати декілька варіантів розвязку.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a = 0, b = 0, c = 0;
+	double arithmetic_mean = 0;
+
+	cout << "Enter three Integer numbers, separated with ENTER button" << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	// Solution #1 : 
+	cout << "Solution 1: " << (a + b + c) / 3 << endl;
+
+	return 0;
+}
