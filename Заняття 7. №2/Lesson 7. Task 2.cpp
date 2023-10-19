fbd357d338beaf2a6e1386a24edc3e87b686a182
readonly <admin@sokteam.com>
2023-10-19 22:50:23 +0200

    Lesson 7. Task 2.

---
+++ Заняття 7. №2/Lesson 7. Task 2.cpp
@@ -0,0 +1,38 @@
+﻿/*
+Lesson 7 | 2) 3 динамічних змінних різного типу.
+Заповнити їх деякими значеннями. Вивести значення динамічних змінних на екран,
+обчислити добуток та середнє арифметичне. Звільнити пам’ять.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	try {
+		int* _varA = new int;
+		long* _varB = new long;
+		double* _varC = new double;
+
+		*_varA = 10;
+		*_varB = 20;
+		*_varC = 30;
+
+		cout << "_varA = " << *_varA << endl;
+		cout << "_varB = " << *_varB << endl;
+		cout << "_varC = " << *_varC << endl;
+
+		cout << "_varA * _varB * _varC = " << *_varA * *_varB * *_varC << endl;
+		cout << "The arithmetic mean of _varA, _varB, _varC = " << (*_varA + *_varB + *_varC) / 3 << endl;
+
+		delete[] _varA;
+		delete[] _varB;
+		delete[] _varC;
+	}
+	catch (bad_alloc) {
+		cout << "Memory allocation was failed." << endl;
+	}
+
+
+	return 0;
+}
