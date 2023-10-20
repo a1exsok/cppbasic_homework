c1239b4f06490f5c500271e52e70e2b5ff54bcc3
readonly <admin@sokteam.com>
2023-10-20 15:35:47 +0200

    Lesson 8. Task 1.

---
+++ Заняття 8. №1/mylibrary.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+int getNumOfDaysPerMonth(int monthNum)
+{
+	switch (monthNum) {
+	case 1:
+		return 31;
+	case 2:
+		return 28;
+	case 3:
+		return 31;
+	case 4:
+		return 30;
+	case 5:
+		return 31;
+	case 6:
+		return 30;
+	case 7:
+		return 31;
+	case 8:
+		return 31;
+	case 9:
+		return 30;
+	case 10:
+		return 31;
+	case 11:
+		return 30;
+	case 12:
+		return 31;
+	}
+}
+
+
+string getNameOfMonth(int monthNumber)
+{
+	switch (monthNumber) {
+	case 1:
+		return "January";
+	case 2:
+		return "February";
+	case 3:
+		return "March";
+	case 4:
+		return "April";
+	case 5:
+		return "May";
+	case 6:
+		return "June";
+	case 7:
+		return "July";
+	case 8:
+		return "August";
+	case 9:
+		return "September";
+	case 10:
+		return "October";
+	case 11:
+		return "November";
+	case 12:
+		return "December";
+	}
+}
