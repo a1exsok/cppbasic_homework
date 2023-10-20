70091f986eb7e6ca2c4391a4168364d5807a7f6b
readonly <admin@sokteam.com>
2023-10-20 19:33:31 +0200

    Lesson 8. Task 3.

---
+++ Заняття 8. №3/Lesson 8. Task 3.cpp
@@ -0,0 +1,14 @@
+﻿/*3.Написати функцію SumParni(N), яка повертає суму
+парних серед цілих чисел N, які були аргументами
+функції.*/
+#include <iostream>
+#include "mylibrary.h"
+
+using namespace std;
+void main()
+{
+	const int size = 4;
+	int arrayA[size] = { 2,1,5,0 };
+
+	cout << SumParni(arrayA, size) << endl;
+}
