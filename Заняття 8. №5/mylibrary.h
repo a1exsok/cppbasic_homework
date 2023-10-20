8b3d44134fca3b0907778a25e8f8741f01de16e2
readonly <admin@sokteam.com>
2023-10-20 20:02:49 +0200

    Lesson 8. Task 5.

---
+++ Заняття 8. №5/mylibrary.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+/*
+5. Написати функцію Avg(N), яка повертає середнє арифметичне
+дробових чисел N, які були аргументами функції.
+*/
+
+float Avg(float args[], int n) {
+	float sum = 0, count = 0;
+	for (int i = 0; i <= n - 1; i++) {
+		if (floor(args[i]) != args[i]) {
+			sum += args[i];
+			count++;
+		}
+	}
+	return sum / count;
+}
+
